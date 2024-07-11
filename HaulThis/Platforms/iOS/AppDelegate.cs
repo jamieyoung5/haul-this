0a68409e14d51bba499633ab51a6e7e0b19ec00c
Jamie Young <jamie5young55@gmail.com>
2024-07-11 04:08:37 +0100

    feat: initial commit

---
+++ HaulThis/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace HaulThis;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
